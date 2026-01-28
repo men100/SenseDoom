28edf778f599050344f37f5bda91810190346883
men100 <men100per@gmail.com>
2026-01-28 21:58:40 +0900

    feat: ILI9341 への描画処理を実装 (タイトル画面の描画を確認)

--- src/doomgeneric_spresense.cpp
+++ src/doomgeneric_spresense.cpp
@@ -1,18 +1,40 @@
+#include <arch/chip/gnssram.h>
 #include <Arduino.h>
 #include <doomtype.h>
 #include <doomgeneric.h>
 #include "spresense_debug.h"
 #include "spresense_sd.h"
+#include "LGFX_SPRESENSE.hpp"
+#include "doomkeys.h"
+
+// USE_RGB565 のとき、pixel_t は uint16_t
+pixel_t* DG_ScreenBuffer = nullptr;
+
+extern LGFX gfx;
 
 extern "C" {
 
 void DG_Init() {
   spresense_debug_init();
   spresense_sd_init();
+  up_gnssram_initialize();
+
+  // 出力バッファは Litte Endian な一方、ILI9341 への入力は Big Endian である必要がある
+  // ここで Byte Swap して、色化けしないようにする
+  gfx.setSwapBytes(true);
+
+  size_t bufferSize = DOOMGENERIC_RESX * DOOMGENERIC_RESY * sizeof(pixel_t);
+  DG_ScreenBuffer = (pixel_t*)malloc(bufferSize);
+
+  if (DG_ScreenBuffer == nullptr) {
+    spresense_printf("DG_Init: Failed to allocate screen buffer!\n");
+    while(1);
+  }
 }
 
 void DG_DrawFrame() {
-  // do nothing
+  // USE_RGB565 により RGB565 バッファが来るので、直接入力できる
+  gfx.pushImage(0, 0, DOOMGENERIC_RESX, DOOMGENERIC_RESY, (uint16_t*)DG_ScreenBuffer);
 }
 
 void DG_SleepMs(uint32_t ms) {
@@ -29,6 +51,7 @@ int DG_GetKey(int* pressed, unsigned char* key) {
   // do nothing
   return 0;
 }
+
 void DG_SetWindowTitle(const char * title) {
   (void)title;
   // do nothing
--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,23 @@
 #include "LGFX_SPRESENSE.hpp"
 
 // 準備したクラスのインスタンスを作成します。
-LGFX display;
+LGFX gfx;
 
 const char* argv[] = {
-  "sensedoom",   // argv[0]
-  "-iwad",       // argv[1]
-  "doom1.wad"    // argv[2]
+  "sensedoom",      // argv[0]
+  "-iwad",          // argv[1]
+  "doom1.wad",      // argv[2]
+  "-gfxmode",       // argv[3]
+  "rgb565"          // argv[4]
 };
 
 void setup() {
   // SPIバスとパネルの初期化を実行すると使用可能になります。
-  display.init();
+  gfx.init();
 
-  display.setBrightness(128); // バックライトの明るさ (0-255)
-  display.setRotation(1);     // ディスプレイの向き (0=縦, 1=横, 2=逆縦, 3=逆横)
+  gfx.setBrightness(128); // バックライトの明るさ (0-255)
+  gfx.setRotation(1);     // ディスプレイの向き (0=縦, 1=横, 2=逆縦, 3=逆横)
   
-  display.fillScreen(TFT_BLACK); // 画面を黒で塗りつぶし
-
-  // テキストを描画
-  display.setCursor(10, 10);
-  display.setFont(&fonts::Font2); // フォントを設定
-  display.setTextColor(TFT_WHITE, TFT_BLACK); // 文字色:白, 背景色:黒
-  
-  display.println("Hello Spresense!");
-  display.println("LovyanGFX + ILI9341");
-
-  // 図形を描画
-  display.drawRect(10, 60, 100, 50, TFT_RED);
-  display.fillRect(120, 60, 100, 50, TFT_GREEN);
-  display.drawCircle(60, 150, 30, TFT_BLUE);
-
   int argc = sizeof(argv) / sizeof(argv[0]);
   doomgeneric_Create(argc, argv);
 }
